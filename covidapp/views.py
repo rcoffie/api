31eca24821aa0166270b88aa71d42da961b9d114
Robert <rcoffie22@gmail.com>
2020-05-19 15:13:24 +0000

    t

--- covidapp/views.py
+++ covidapp/views.py
@@ -5,7 +5,14 @@ import json
 
 
 def index(request):
-  api_request = requests.get('https://api.covid19api.com/summary')
+ # api_request = requests.get('https://mazitekgh.com/covid19/v1/')
+  ghana_request = requests.get('https://corona.lmao.ninja/v2/countries/Ghana')
+  ghana = json.loads(ghana_request.content)
+  
+  
+  api_request = requests.get('https://corona.lmao.ninja/v2/countries?sort=country')
   api = json.loads(api_request.content)
-  context = {'api':api}
+  
+  
+  context = {'api':api,'ghana':ghana}
   return render(request,'index.html',context)
